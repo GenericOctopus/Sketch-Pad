29e2efc3198ba6f7b661cccad0d9543dda071e29
GenericOctopus <genericoctopus@gmail.com>
2022-12-22 21:02:21 -0500

    can reset pad, and set a new size for pad

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const sketchPad = document.querySelector('.pad');
 
 
 let initSize = 8;
+let newSize = initSize;
 
 
 //function to create grid blocks
@@ -17,6 +18,9 @@ function createBlock(blockSize) {
     return block;
 }
 
+
+//Takes an int for a pad size, then generates a grid of blocks
+
 function setPad(padSize) {
 
     let padSizeSquared = padSize * padSize;
@@ -29,12 +33,34 @@ function setPad(padSize) {
     }
 }
 
+
+//Initial sketch pad
 setPad(initSize);
 
 
+//Listens for the mouse to hover over a block, then fills it
 sketchPad.addEventListener('mouseover', function(e){
     if(e.target.matches('.blockStyle')){
         e.target.classList.add('filledBlock');
     }
 });
+
+//Function for a button to clear any "drawings" on current pad
+function clearPad(){
+    while (sketchPad.hasChildNodes()){
+        sketchPad.removeChild(sketchPad.firstChild);
+    }
+
+    setPad(newSize);
+}
+
+
+//Sets the pad to a new grid size
+function setPadSize(){
+    let inputSize = document.getElementById("padSize").value;
+    
+    newSize = inputSize;
+
+    clearPad();
+}
 
